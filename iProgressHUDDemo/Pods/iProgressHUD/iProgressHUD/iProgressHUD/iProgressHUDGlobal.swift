e627a2b4d103734cf76091779033aa7a1354805e
Rudi Farkas <rudi.farkas@gmail.com>
2023-07-18 21:36:36 +0200

    update iProgressHUDDemo - remove warnings	 re deprecated keyWindow; bump version to 1.2.0

--- iProgressHUDDemo/Pods/iProgressHUD/iProgressHUD/iProgressHUD/iProgressHUDGlobal.swift
+++ iProgressHUDDemo/Pods/iProgressHUD/iProgressHUD/iProgressHUD/iProgressHUDGlobal.swift
@@ -9,23 +9,6 @@
 import Foundation
 import UIKit
 
-internal extension UIApplication {
-    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-        if let nav = base as? UINavigationController {
-            return topViewController(base: nav.visibleViewController)
-        }
-        if let tab = base as? UITabBarController {
-            if let selected = tab.selectedViewController {
-                return topViewController(base: selected)
-            }
-        }
-        if let presented = base?.presentedViewController {
-            return topViewController(base: presented)
-        }
-        return base
-    }
-}
-
 public extension UIView {
     
     private struct AssociatedKeys {
